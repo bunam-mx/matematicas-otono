982098da7a15a07fa3c2a675f66cc9b614b43920
Omar Terrazas <terrazas.omar@gmail.com>
2024-06-10 19:37:32 -0600

    Ajustes en Header y Footer

--- src/Components/HeaderBlock/HeaderBlock.jsx
+++ src/Components/HeaderBlock/HeaderBlock.jsx
@@ -5,11 +5,11 @@ function HeaderBlock() {
     <header className="header">
       <div className="header__container">
         <ul>
-          <li><a href="https://www.unam.mx/" target="_blank" rel="noreferrer"><img src="images/unam.png" alt="Universidad Nacional Autónoma de México" width={60} /></a></li>
-          <li><a href="https://www.fciencias.unam.mx/node/5" target="_blank" rel="noreferrer"><img src="images/fac_ciencias.png" alt="Facultad de Ciencias" width={60} /></a></li>
-          <li><a href="http://enp.unam.mx/" target="_blank" rel="noreferrer"><img src="images/enp.png" alt="Escuela Nacional Preparatoria" width={60} /></a></li>
-          <li><a href="https://www.cch.unam.mx/" target="_blank" rel="noreferrer"><img src="images/cch.png" alt="Colegio de Ciencias y Humanidades" width={60} /></a></li>
-          <li><a href="https://www.bunam.unam.mx/" target="_blank" rel="noreferrer"><img src="images/bunam.png" alt="Bachillerato a distancia B@UNAM" width={90} /></a></li>
+          <li><a href="https://www.unam.mx/" target="_blank" rel="noreferrer"><img src={`${import.meta.env.BASE_URL}images/unam.png`} alt="Universidad Nacional Autónoma de México" width={60} /></a></li>
+          <li><a href="https://www.fciencias.unam.mx/node/5" target="_blank" rel="noreferrer"><img src={`${import.meta.env.BASE_URL}images/fac_ciencias.png`} alt="Facultad de Ciencias" width={60} /></a></li>
+          <li><a href="http://enp.unam.mx/" target="_blank" rel="noreferrer"><img src={`${import.meta.env.BASE_URL}images/enp.png`} alt="Escuela Nacional Preparatoria" width={60} /></a></li>
+          <li><a href="https://www.cch.unam.mx/" target="_blank" rel="noreferrer"><img src={`${import.meta.env.BASE_URL}images/cch.png`} alt="Colegio de Ciencias y Humanidades" width={60} /></a></li>
+          <li><a href="https://www.bunam.unam.mx/" target="_blank" rel="noreferrer"><img src={`${import.meta.env.BASE_URL}images/bunam.png`} alt="Bachillerato a distancia B@UNAM" width={90} /></a></li>
         </ul>
       </div>
     </header>
