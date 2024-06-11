7a7faf659d78d90749261907b1635dc4fc13bb65
Omar Terrazas <terrazas.omar@gmail.com>
2024-06-10 20:04:33 -0600

    Se modifican rutas en Call to register

--- src/Components/Home/CallToRegisterBlock/CallToRegisterBlock.jsx
+++ src/Components/Home/CallToRegisterBlock/CallToRegisterBlock.jsx
@@ -6,10 +6,10 @@ function CallToRegisterBlock() {
     <div className="calltoregister-block">
       <h2>Registro abierto</h2>
       <p className="calltoregister-block__register">
-        <NavLink to="/speaker/register/">Ponente</NavLink>
-        <NavLink to="/assistant/register/">Asistente</NavLink>
+        <NavLink to="/register/speaker/">Ponente</NavLink>
+        <NavLink to="/register/assistant/">Asistente</NavLink>
       </p>
-      <p className="calltoregister-block__guidelines"><a href="/mate-otono-2024/docs/lineamientos.pdf" download="lineamientos.pdf">Lineamientos para la elaboración de ponencias</a></p>
+      <p className="calltoregister-block__guidelines"><a href={`${import.meta.env.BASE_URL}docs/lineamientos.pdf`} download="lineamientos.pdf">Lineamientos para la elaboración de ponencias</a></p>
     </div>
   );
 }
