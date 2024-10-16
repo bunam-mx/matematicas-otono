72ddf25f4c58a68d2a84cb8cd6b934fceb194338
Omar Terrazas <terrazas.omar@gmail.com>
2024-10-16 17:10:38 -0600

    Se realizan cambios en el programa

--- src/Components/Home/ScheduleBlock/November7th.jsx
+++ src/Components/Home/ScheduleBlock/November7th.jsx
@@ -8,13 +8,13 @@ function November7th() {
         <tbody>
           <tr>
             <td>08:00 - 09:00</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>Registro</h4>
             </td>
           </tr>
           <tr>
             <td>09:00 - 09:40</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <div>
                 <h4>Inauguración</h4>
                 <ul>
@@ -32,35 +32,33 @@ function November7th() {
                 <p>&nbsp;</p>
                 <ul>
                   <li>Dr. Víctor Manuel Velázquez Aguilar</li>
+                  <li>Dr. Manuel J. Falconi Magaña</li>
                   <li>Biol. María Dolores Valle Martínez</li>
-                  <li>Dr. Benjamín Barajas Sánchez</li>
                   <li>Dra. Guadalupe Vadillo</li>
+                  <li>Dr. Benjamín Barajas Sánchez</li>
                 </ul>
-                <p className="schedule__item__moderator">
-                  Modera: Dr. Manuel Jesús Falconi Magaña
-                </p>
               </div>
               <p className="schedule__item__venue">Auditorio Carlos Graef</p>
             </td>
           </tr>
           <tr>
-            <td>09:45 - 10:30</td>
-            <td colSpan="4">
-              <h4>Historia de las Matemáticas</h4>
+            <td>09:45 - 10:45</td>
+            <td colSpan="5">
+              <h4>Lectura e interpretación &quot;moderna&quot; de un texto &quot;clásico&quot;</h4>
               <h5>Conferencia magistral</h5>
               <p>Dr. Carlos Álvarez Jiménez</p>
               <p className="schedule__item__venue">Auditorio Carlos Graef</p>
             </td>
           </tr>
           <tr>
-            <td>10:30 - 11:30</td>
-            <td colSpan="4">
+            <td>11:00 - 12:30</td>
+            <td colSpan="5">
               <h4>La historia de las matemáticas, una pieza fundamental para su enseñanza</h4>
               <h5>Mesa redonda</h5>
               <ul>
                 <li>M. en C. José Rafael Martínez Enríquez</li>
                 <li>M. en C. Javier Martín Zariñán Sánchez</li>
-                <li>M. en C. María del Socorro Rivera Casales</li>
+                <li>Dra. María del Socorro Rivera Casales</li>
                 <li>Dra. Carmen Martinez Adame</li>
               </ul>
               <p className="schedule__item__moderator">
@@ -70,88 +68,50 @@ function November7th() {
             </td>
           </tr>
           <tr>
-            <td>11:30 - 12:00</td>
-            <td colSpan="4">
+            <td>12:30 - 13:00</td>
+            <td colSpan="5">
               <h4>Receso</h4>
             </td>
           </tr>
           <tr>
-            <td>12:00 - 12:30</td>
-            <td>
+            <td>13:00 - 13:30</td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
             </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
-            <td rowSpan="4">
+            <td rowSpan="2">
               <h4>Taller de Geogebra</h4>
               <ul>
                 <li>Dr. Marcos Campos Nava</li>
-                <li>Agustín Torres</li>
+                <li>Dr. Agustín Torres</li>
               </ul>
               <p className="schedule__item__venue">Aula 5 Amoxcalli</p>
             </td>
           </tr>
           <tr>
-            <td>12:30 - 13:00</td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 1</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
-            </td>
-          </tr>
-          <tr>
-            <td>13:00 - 13:30</td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 1</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
-            </td>
-          </tr>
-          <tr>
             <td>13:30 - 14:00</td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
             </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
+            <td colSpan="2"> 
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
           </tr>
           <tr>
             <td>14:00 - 16:00</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>Comida</h4>
             </td>
           </tr>
           <tr>
             <td>16:00 - 17:00</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>IA generativa para la enseñanza de las matemáticas</h4>
               <h5>Conferencia Magistral</h5>
               <p>Dra. Nelly Rigaud Téllez</p>
@@ -166,19 +126,27 @@ function November7th() {
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
+              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
+              <p className="schedule__item__venue">Leonila Vazquez</p>
             </td>
             <td rowSpan="4">
               <h4>Taller de Geogebra</h4>
               <ul>
                 <li>Dr. Marcos Campos Nava</li>
-                <li>Agustín Torres</li>
+                <li>Dr. Agustín Torres</li>
               </ul>
               <p className="schedule__item__venue">Aula 5 Amoxcalli</p>
+            </td>
+            <td rowSpan="4">
+              <h4>Taller de Podcast</h4>
+              <ul>
+                <li>&nbsp;</li>
+                <li>&nbsp;</li>
+              </ul>
+              <p className="schedule__item__venue">Aula 6 Amoxcalli</p>
             </td>
           </tr>
           <tr>
@@ -189,11 +157,11 @@ function November7th() {
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
+              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
+              <p className="schedule__item__venue">Leonila Vazquez</p>
             </td>
           </tr>
           <tr>
@@ -204,11 +172,11 @@ function November7th() {
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
+              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
+              <p className="schedule__item__venue">Leonila Vazquez</p>
             </td>
           </tr>
           <tr>
@@ -219,11 +187,11 @@ function November7th() {
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
+              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
+              <p className="schedule__item__venue">Leonila Vazquez</p>
             </td>
           </tr>
         </tbody>
--- src/Components/Home/ScheduleBlock/November8th.jsx
+++ src/Components/Home/ScheduleBlock/November8th.jsx
@@ -8,14 +8,14 @@ function November8th() {
         <tbody>
           <tr>
             <td>08:00 - 09:00</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>Registro</h4>
             </td>
           </tr>
           <tr>
             <td>09:00 - 10:00</td>
-            <td colSpan="4">
-              <h4>Nueva Escuela Mexicana ¿qué esperamos en el bachillerato?</h4>
+            <td colSpan="5">
+              <h4>Sobre la Nueva Escuela Mexicana</h4>
               <h5>Conferencia magistral</h5>
               <ul>
                 <li>Dr. Alejandro Díaz Barriga</li>
@@ -26,12 +26,12 @@ function November8th() {
           </tr>
           <tr>
             <td>10:00 - 11:30</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>IA generativa en la  enseñanza y aprendizaje de las matemáticas</h4>
               <h5>Mesa redonda</h5>
               <ul>
                 <li>Dr. Alfonso Ochoa Hofmann</li>
-                <li>Dr. Ismael Everardo Bárcenas  Patiño</li>
+                <li>Dr. Ismael Everardo Bárcenas Patiño</li>
                 <li>Dra. Verónica Esther Arriola Ríos</li>
               </ul>
               <p className="schedule__item__moderator">Modera: Dra. Guadalupe Vadillo</p>
@@ -40,21 +40,17 @@ function November8th() {
           </tr>
           <tr>
             <td>11:30 - 12:00</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>Receso</h4>
             </td>
           </tr>
           <tr>
             <td>12:00 - 12:30</td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
             </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
@@ -63,92 +59,57 @@ function November8th() {
               <ul>
                 <li>Mtro. Omar Terrazas Razo</li>
               </ul>
-              <p className="schedule__item__venue">Aula 5 Amoxcalli</p>
+              <p className="schedule__item__venue">Aula 6 Amoxcalli</p>
             </td>
           </tr>
           <tr>
             <td>12:30 - 13:00</td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
             </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
+            <td colSpan="2" rowSpan="2">
+              <h4>Silvia Aguilar Martínez<br />NEM</h4>
               <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
           </tr>
           <tr>
             <td>13:00 - 13:30</td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
           </tr>
           <tr>
             <td>13:30 - 14:00</td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
             </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
+            <td colSpan="2">
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
           </tr>
           <tr>
             <td>14:00 - 16:00</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>Comida</h4>
             </td>
           </tr>
           <tr>
-            <td>16:00 - 17:00</td>
-            <td colSpan="4">
-              <h4>Principios de la Nueva Escuela Mexicana en la EMS</h4>
+            <td>16:00 - 17:30</td>
+            <td colSpan="5">
+              <h4>Nueva Escuela Mexicana ¿qué esperamos en el bachillerato?</h4>
               <h5>Mesa redonda</h5>
               <ul>
                 <li>Dr. Javier Lezama Andalón</li>
-                <li>Dra. Augus Edóc Zaira Molina Xerónimo</li>
-                <li></li>
+                <li>Dr. José Lorenzo Sánchez Alavez</li>
+                <li>Dra. Zaira Molina Xeronimo</li>
                 <li>Dra.Nadia Gil Ruiz</li>
               </ul>
               <p className="schedule__item__moderator">Modera: Dra. Adriana Gomez Reyes y Dra. Leticia Sánchez López</p>
               <p className="schedule__item__venue">Auditorio Carlos Graef</p>
-            </td>
-          </tr>
-          <tr>
-            <td>17:00 - 17:30</td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 1</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
-            </td>
-            <td rowSpan="4">
-              <h4>Taller IA Generativa</h4>
-              <p>&nbsp;</p>
             </td>
           </tr>
           <tr>
@@ -159,11 +120,15 @@ function November8th() {
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
+              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
+              <p className="schedule__item__venue">Leonila Vázquez</p>
+            </td>
+            <td colSpan="2" rowSpan="4">
+              <h4>Taller IA Generativa</h4>
+              <p>Dr. Leonardo Martínez Sandoval</p>
             </td>
           </tr>
           <tr>
@@ -174,31 +139,31 @@ function November8th() {
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
+              <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
             <td>
               <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 3</p>
+              <p className="schedule__item__venue">Leonila Vázquez</p>
             </td>
           </tr>
           <tr>
             <td>18:30 - 19:00</td>
             <td>
-              <h4>Ponencia</h4>
+              <h4>Taller<br />Qué es Integrando</h4>
               <p className="schedule__item__venue">Sotero Prieto 1</p>
-            </td>
-            <td>
-              <h4>Ponencia</h4>
-              <p className="schedule__item__venue">Sotero Prieto 2</p>
             </td>
             <td>
               <h4>Ponencia</h4>
               <p className="schedule__item__venue">Sotero Prieto 3</p>
             </td>
+            <td>
+              <h4>Ponencia</h4>
+              <p className="schedule__item__venue">Leonila Vázquez</p>
+            </td>
           </tr>
           <tr>
             <td>19:00 - 19:30</td>
-            <td colSpan="4">
+            <td colSpan="5">
               <h4>Clausura</h4>
               <ul>
                 <li>Dr. Manuel Jesús Falconi Magaña</li>
